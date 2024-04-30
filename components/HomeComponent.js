cfa69d9a5b8d2deeedf2799cc3499fc6c510f195
SOURADEEP HAZRA <V22089@students.iitmandi.ac.in>
2024-04-30 17:37:05 +0530

    updated ui

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -3,7 +3,8 @@ import React from "react";
 
 const HomeComponent = () => {
   return (
-    <div className=" mt-10 px-20 fle3x flex-col gap-7">
+    <div className="   w-full h-full">
+      {/* <VideoPlayer src="/Video.mp4" /> */}
       {/* <Image
         src={"/image.png"}
         width={300}
@@ -12,11 +13,21 @@ const HomeComponent = () => {
         className=" w-full h-full"
       /> */}
       {/* inser vidio her  take all width*/}
-      <p className=" text-3xl text-center mb-5">VineNet: Grasp of the Grapes</p>
-      <video width="100%" controls>
+      {/* <p className=" text-3xl text-center mb-5">VineNet: Grasp of the Grapes</p> */}
+      <video
+        width="100%"
+        height={"100%"}
+        autoPlay
+        muted
+        loop
+        className="relative"
+      >
         <source src="/Video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <p className=" absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2 text-3xl text-center text-white font-bold mb-5">
+        VineNet: Grasp of the Grapes
+      </p>
     </div>
   );
 };
